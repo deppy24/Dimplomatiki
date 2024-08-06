c502b59336f3778be4d6bdf94608364142601cbf
nikos iosifidis <nikos_ios@outlook.com>
2024-08-06 22:36:10 +0300

    commit

--- React.try/industrial_rows/src/DashboardsConstructors/Home.jsx
+++ React.try/industrial_rows/src/DashboardsConstructors/Home.jsx
@@ -1,13 +1,9 @@
 //access via DBOARD path
 //import { Size } from 'devextreme-react/cjs/bar-gauge';
-import React, {useState,useEffect} from 'react'
+import React, {useState} from 'react'
 import 
-{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill, BsFillClockFill}
+{ BsPeopleFill, BsFillBellFill, BsFillClockFill}
  from 'react-icons/bs'
- import 
- { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } 
-from 'recharts';
-import uuid from "react-uuid";
 //import mainnotes from '../Notes part/mainnotes';
 //import sidebarnotes from '../Notes part/sidebarnotes';
 
