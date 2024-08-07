dc897d66ca2973648e0ffaaf2752e7c0a35ba0ff
nikos iosifidis <nikos_ios@outlook.com>
2024-08-07 22:48:38 +0300

    console error fixes

--- React.try/industrial_rows/src/DashboardsConstructors/Configuration.jsx
+++ React.try/industrial_rows/src/DashboardsConstructors/Configuration.jsx
@@ -1,91 +1,93 @@
-
-
-import Sidebar from "../DashboardsConstructors/Sidebar.jsx";
-import '../CSS FILES/dashpage.css'
-import './Header.jsx'
-import React, { useState } from 'react'
-import GoogleMapReact from "google-map-react";
-import { Icon } from "@iconify/react";
-import locationIcon from "@iconify/icons-mdi/map-marker";
-import { Link } from "react-router-dom";
+import Sidebar from '../DashboardsConstructors/Sidebar.jsx';
+import '../CSS FILES/dashpage.css';
+import './Header.jsx';
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Icon } from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/map-marker';
+import { Link } from 'react-router-dom';
 import '../CSS FILES/Map.css';
-import { Button, Tooltip } from 'bootstrap';
 
 function Configuration() {
-    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+	const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
-    const OpenSidebar = () => {
-      setOpenSidebarToggle(!openSidebarToggle)
-    }
+	const OpenSidebar = () => {
+		setOpenSidebarToggle(!openSidebarToggle);
+	};
 
+	const location = [
+		{
+			name: 'Διεθνές Πανεπιστήμιο Ελλάδας',
+			address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
+			lat: 40.53623903334093,
+			lng: 23.009268511780395,
+		},
+		{
+			name: 'Πανεπιστήμιο Μακεδονίας',
+			address: 'Εγνατία 156, Θεσσαλονίκη 546 36',
+			lat: 40.625249019392484,
+			lng: 22.960205055964213,
+		},
+		{
+			name: 'Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης',
+			address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
+			lat: 40.53630426420577,
+			lng: 23.00919340992946,
+		},
+		{
+			name: 'Διεθνές Πανεπιστήμιο Ελλάδας',
+			address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
+			lat: 40.53623903334093,
+			lng: 23.009268511780395,
+		},
+	];
 
+	const LocationPin = ({ text }) => (
+		<div className='pin'>
+			<Icon
+				icon={locationIcon}
+				className='pin-icon'
+			/>
+			<p className='pin-text'>{text}</p>
+		</div>
+	);
 
-    const location = [
-        {
-            name: 'Διεθνές Πανεπιστήμιο Ελλάδας',
-            address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
-            lat: 40.53623903334093,
-            lng:  23.009268511780395,
-        },
-        {
-            name: 'Πανεπιστήμιο Μακεδονίας',
-            address: 'Εγνατία 156, Θεσσαλονίκη 546 36',
-            lat: 40.625249019392484, 
-            lng:  22.960205055964213,
-        },
-        {
-            name: 'Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης',
-            address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
-            lat: 40.53630426420577, 
-            lng:  23.00919340992946,
-        },
-        {
-          name: 'Διεθνές Πανεπιστήμιο Ελλάδας',
-          address: '14th km Thessaloniki, Νέα Μουδανιά 570 01',
-          lat: 40.53623903334093,
-          lng:  23.009268511780395,
-      }
-    
-    ];
-    
-    
-    
-    const LocationPin = ({text}) => (
-      <div className="pin">
-        <Icon icon={locationIcon} className="pin-icon" />
-        <p className="pin-text">{text}</p>
-      </div>
-    );
-    
-    
-   
-    return(
-        <div className='grid-container'>
-            <Sidebar className='sidebar' openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-            <div className='main-conatiner'>
-            
- 
-                <div className="map">
-                <div className='header'>
-                <h2 className="map-h2">Enter your data device</h2>
-                <div className='button'>
-                    <Link to="/DBOARD"><button className='btn-goto'>Go to Dashboards</button></Link>
-                </div>
-                </div>
-                <div className="google-map" >
-                    <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyDsglBBOSMgYp9Wn7hPnC4HiYfy5eXintM" }}
-                    defaultCenter={location[0]}
-                    defaultZoom={10}>
-                    {location.map((loc) => (
-                    <LocationPin lat={loc.lat} lng={loc.lng} text={loc.address} name={loc.buildingname }/>
-                    ))}
-                    </GoogleMapReact>
-                </div>
-                </div>
-            
-            </div>
-        </div>
-    )
+	return (
+		<div className='grid-container'>
+			<Sidebar
+				className='sidebar'
+				openSidebarToggle={openSidebarToggle}
+				OpenSidebar={OpenSidebar}
+			/>
+			<div className='main-conatiner'>
+				<div className='map'>
+					<div className='header'>
+						<h2 className='map-h2'>Enter your data device</h2>
+						<div className='button'>
+							<Link to='/DBOARD'>
+								<button className='btn-goto'>Go to Dashboards</button>
+							</Link>
+						</div>
+					</div>
+					<div className='google-map'>
+						<GoogleMapReact
+							bootstrapURLKeys={{ key: 'AIzaSyDsglBBOSMgYp9Wn7hPnC4HiYfy5eXintM' }}
+							defaultCenter={location[0]}
+							defaultZoom={10}>
+							{location.map((loc, index) => (
+								<LocationPin
+									key={index}
+									lat={loc.lat}
+									lng={loc.lng}
+									text={loc.address}
+									name={loc.buildingname}
+								/>
+							))}
+						</GoogleMapReact>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Configuration;
--- React.try/industrial_rows/src/DashboardsConstructors/Home.jsx
+++ React.try/industrial_rows/src/DashboardsConstructors/Home.jsx
@@ -1,83 +1,71 @@
 //access via DBOARD path
 //import { Size } from 'devextreme-react/cjs/bar-gauge';
-import React, {useState} from 'react'
-import 
-{ BsPeopleFill, BsFillBellFill, BsFillClockFill}
- from 'react-icons/bs'
+import React, { useState } from 'react';
+import { BsPeopleFill, BsFillBellFill, BsFillClockFill } from 'react-icons/bs';
 //import mainnotes from '../Notes part/mainnotes';
 //import sidebarnotes from '../Notes part/sidebarnotes';
 
-
-
 //Gets current Date and Time (the moment we enter the page or refresh not continuously)
 function getDate() {
-  const today = new Date();
-  const month = today.getMonth() + 1;
-  const year = today.getFullYear();
-  const date = today.getDate(); <br/>
-  const hour = today.getHours();
-  const minutes = today.getMinutes();
-  const seconds = today.getSeconds();
-  return `${"CURRENT DATE:"} ${month}/${date}/${year}                              ` +
-   `${"CURRENT TIME:"}${hour}:${minutes}:${seconds}`;
+	const today = new Date();
+	const month = today.getMonth() + 1;
+	const year = today.getFullYear();
+	const date = today.getDate();
+	const hour = today.getHours();
+	const minutes = today.getMinutes();
+	const seconds = today.getSeconds();
+	return `${'CURRENT DATE:'} ${month}/${date}/${year}                              ` + `${'CURRENT TIME:'}${hour}:${minutes}:${seconds}`;
 }
-
 
 function Home() {
-  const[currentDate,setCurrentDate] = useState(getDate());
+	const [currentDate, setCurrentDate] = useState(getDate());
 
- 
-  
-  return (
-    <main className='main-container'>
-        <div className='main-title'>
-            <h3>DASHBOARD</h3>
-        </div>
+	return (
+		<main className='main-container'>
+			<div className='main-title'>
+				<h3>DASHBOARD</h3>
+			</div>
 
-        <div className='main-cards'>
-            <div className='card'>
-                <div className='card-inner'>
-                    <BsFillClockFill className='one_card_icon'/>
-                </div>
-                <h2 className='h2date'> TIME</h2>
-                <h1 className='Datetime'>{currentDate}</h1>
-            </div>
-            
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>CUSTOMERS</h3>
-                    <BsPeopleFill className='card_icon'/>
-                </div>
-                <h1>15</h1>
-            </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>ALERTS</h3>
-                    <BsFillBellFill className='card_icon'/>
-                </div>
-                <h1>42</h1>
-            </div>
-        </div>
+			<div className='main-cards'>
+				<div className='card'>
+					<div className='card-inner'>
+						<BsFillClockFill className='one_card_icon' />
+					</div>
+					<h2 className='h2date'> TIME</h2>
+					<h1 className='Datetime'>{currentDate}</h1>
+				</div>
 
-        <div className="about-section">
-            <br/><h1>ΑΝΑΚΟΙΝΩΣΕΙΣ </h1> <br/>
-            <div class="flexbox">
-                <span class="textbox1">Ανακοίνωση 1</span> <br/><br/>
-                <span class="textbox1">Ανακοίνωση 2</span>
-            </div>
-            
-            
-        </div>
-        <div className="notes-section">
-            <br/><h1>ΣΗΜΕΙΩΣΕΙΣ</h1> 
-            
-            
-            
-        </div>
+				<div className='card'>
+					<div className='card-inner'>
+						<h3>CUSTOMERS</h3>
+						<BsPeopleFill className='card_icon' />
+					</div>
+					<h1>15</h1>
+				</div>
+				<div className='card'>
+					<div className='card-inner'>
+						<h3>ALERTS</h3>
+						<BsFillBellFill className='card_icon' />
+					</div>
+					<h1>42</h1>
+				</div>
+			</div>
 
-    </main>
-  )
+			<div className='about-section'>
+				<br />
+				<h1>ΑΝΑΚΟΙΝΩΣΕΙΣ </h1> <br />
+				<div className='flexbox'>
+					<span className='textbox1'>Ανακοίνωση 1</span> <br />
+					<br />
+					<span className='textbox1'>Ανακοίνωση 2</span>
+				</div>
+			</div>
+			<div className='notes-section'>
+				<br />
+				<h1>ΣΗΜΕΙΩΣΕΙΣ</h1>
+			</div>
+		</main>
+	);
 }
 
-export default Home
-
+export default Home;
